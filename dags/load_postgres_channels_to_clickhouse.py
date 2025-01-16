bc0755488eb9cc3613c67955f94053128fcfee29
Kavian <kavianam@gmail.com>
2025-01-16 12:39:40 +0330

    feat: add validation to ClickHouse channels data

--- dags/load_postgres_channels_to_clickhouse.py
+++ dags/load_postgres_channels_to_clickhouse.py
@@ -1,11 +1,9 @@
-from datetime import timedelta
-
-import pendulum
 from airflow import DAG
 from airflow.models import Variable
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
 from airflow_clickhouse_plugin.hooks.clickhouse import ClickHouseHook
+from pendulum import datetime, duration
 
 
 def get_clickhouse_hook():
@@ -49,6 +47,20 @@ def incremental_load_data_from_postgres():
         WHERE start_date > (SELECT toString(MAX(start_date)) FROM bronze.channels);
     """)
 
+def data_validity_check():
+    hook = get_clickhouse_hook()
+    postgres_count = hook.execute(f"""
+        SELECT COUNT(*)
+        FROM postgresql('{Variable.get('postgres_host')}', '{Variable.get("postgres_csv_db")}', 'channels', '{Variable.get("postgres_user")}', '{Variable.get("postgres_password")}');
+    """)
+    clickhouse_count = hook.execute(f"""
+        SELECT COUNT(*)
+        FROM bronze.channels3;
+    """)
+
+    if postgres_count[0][0] != clickhouse_count[0][0]:
+        raise ValueError(f"Postgres count: {postgres_count[0][0]} - ClickHouse count: {clickhouse_count[0][0]}")
+
 
 default_args = {
     'owner': 'Kavian',
@@ -57,7 +69,7 @@ default_args = {
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 3,
-    'retry_delay': timedelta(minutes=5),
+    'retry_delay': duration(minutes=1),
     'catchup': False,
 }
 
@@ -65,7 +77,7 @@ with DAG(
     'load_postgres_channels_to_clickhouse',
     'Load channels data in Postgres into ClickHouse',
     '@daily',
-    start_date=pendulum.datetime(2025, 1, 11, tz="Asia/Tehran"),
+    start_date=datetime(2025, 1, 11, tz="Asia/Tehran"),
     tags=['clickhouse', 'postgres', 'bronze'],
 ):
     START = EmptyOperator(task_id='START')
@@ -74,11 +86,22 @@ with DAG(
     create_table = PythonOperator(
         task_id='create_clickhouse_channels_table',
         python_callable=create_clickhouse_table,
+        retries=3,
+        retry_delay=duration(minutes=1),
     )
 
     load_channels_data = PythonOperator(
         task_id='incremental_load_postgres_channels_to_clickhouse',
         python_callable=incremental_load_data_from_postgres,
+        retries=3,
+        retry_delay=duration(minutes=1),
     )
 
-    START >> create_table >> load_channels_data >> END
+    check_data_validity = PythonOperator(
+        task_id='data_validity_check',
+        python_callable=data_validity_check,
+        retries=3,
+        retry_delay=duration(minutes=1),
+    )
+
+    START >> create_table >> load_channels_data >> check_data_validity >> END
