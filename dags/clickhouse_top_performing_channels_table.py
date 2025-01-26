c7436e0fff221376da62c9a79cd24dea05cacb4c
Majid Darigh <majiddarigh@gmail.com>
2025-01-26 15:22:42 +0330

    update code to create mv

--- dags/clickhouse_top_performing_channels_table.py
+++ dags/clickhouse_top_performing_channels_table.py
@@ -1,8 +1,65 @@
 from datetime import datetime, timedelta
 
-from airflow.decorators import dag, task
-from airflow.models.connection import Connection
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
 from airflow_clickhouse_plugin.hooks.clickhouse import ClickHouseHook
+
+
+def get_conn_clickhouse():
+    return ClickHouseHook(clickhouse_conn_id="my_clickhouse_database")
+
+
+def create_table_clickhouse():
+    hook = get_conn_clickhouse()
+    hook.execute("CREATE DATABASE IF NOT EXISTS gold;")
+    hook.execute("DROP TABLE IF EXISTS gold.top_performing_channels;")
+    hook.execute(
+        """
+        CREATE TABLE IF NOT EXISTS gold.top_performing_channels
+        (
+            channel_userid String,
+            total_followers_count UInt64,
+            total_video_visits UInt64,
+            total_video_count UInt64
+        )
+        ENGINE = AggregatingMergeTree
+        ORDER BY (channel_userid);
+        """
+    )
+
+
+def create_materialized_view_clickhouse():
+    hook = get_conn_clickhouse()
+    hook.execute("DROP TABLE IF EXISTS gold.top_performing_channels_mv;")
+    hook.execute(
+        """
+        CREATE MATERIALIZED VIEW IF NOT EXISTS gold.top_performing_channels_mv
+        TO gold.top_performing_channels AS
+        SELECT
+            channel_userid,
+            SUM(channel_followers_count) AS total_followers_count,
+            SUM(channel_total_video_visit) AS total_video_visits,
+            COUNT(video_id) AS total_video_count
+        FROM silver.Videos_channels_OBT
+        GROUP BY channel_userid;
+        """
+    )
+
+
+def initialize_table():
+    hook = get_conn_clickhouse()
+    hook.execute(
+        """
+        INSERT INTO gold.top_performing_channels
+        SELECT
+            channel_userid,
+            sum(channel_followers_count) AS total_followers_count,
+            sum(channel_total_video_visit) AS total_video_visits,
+            count(video_id) AS total_video_count
+        FROM silver.Videos_channels_OBT
+        GROUP BY channel_userid;
+        """
+    )
 
 
 default_args = {
@@ -14,74 +71,30 @@ default_args = {
 }
 
 
-@dag(
+with DAG(
     dag_id='top_performing_channels',
     description='a dag to create top performing channels report',
     schedule='@once',
     default_args=default_args,
     catchup=False,
-    tags=['clickhouse', 'channel-Rep']        
-)
-def top_perform_ch():
-    from pprint import pprint
-    from clickhouse_driver import Client
-    clickhouse_engine = ClickHouseHook(clickhouse_conn_id='clickhouse_tcp')    
+    tags=['gold', 'clickhouse']        
+) as dag:
+
+    create_table_clickhouse_task = PythonOperator(
+        task_id="create_table_top_performing_ch",
+        python_callable=create_table_clickhouse,
+    )
+
+    create_mv_clickhouse_task = PythonOperator(
+        task_id="create_mv",
+        python_callable=create_materialized_view_clickhouse,
+    )
+
+    initialize_table_task = PythonOperator(
+        task_id="initialize_table_top_performing_ch",
+        python_callable=initialize_table,
+    )
 
 
-    @task
-    def print_context(**context):
-        pprint(context)
-
-    @task
-    def top_performing_channels():
-        client = Client('82.115.20.70', port=9000)
-        # Queries
-        create_db_query = "CREATE DATABASE IF NOT EXISTS gold"
-
-        drop_table_query = """
-        DROP TABLE IF EXISTS gold.top_performing_channels
-        """
-
-        create_table_query = """
-        CREATE TABLE gold.top_performing_channels
-        (
-            channel_userid String,
-            total_followers_count UInt64,
-            total_video_visits UInt64,
-            total_video_count UInt64
-        )
-        ENGINE = AggregatingMergeTree
-        ORDER BY (channel_userid)
-        SETTINGS index_granularity = 8192;
-        """
-
-        insert_data_query = """
-        INSERT INTO gold.top_performing_channels
-        SELECT
-            channel_userid,
-            sum(channel_followers_count) AS total_followers_count,
-            sum(channel_total_video_visit) AS total_video_visits,
-            count(video_id) AS total_video_count
-        FROM silver.Videos_channels_OBT
-        GROUP BY channel_userid;
-        """
-
-
-
-        client.execute(create_db_query)
-        print("Database 'gold' ensured to exist.")
-            
-        client.execute(drop_table_query)
-        print("Dropped table 'top_performing_channels' if it existed.")
-            
-        client.execute(create_table_query)
-        print("Created table 'top_performing_channels'.")
-            
-        client.execute(insert_data_query)
-        print("Inserted aggregated data into 'top_performing_channels'.")
-            
-            
-    print_context() >> top_performing_channels()
-
-
-top_perform_ch()
+create_table_clickhouse_task >> create_mv_clickhouse_task
+create_mv_clickhouse_task >> initialize_table_task
