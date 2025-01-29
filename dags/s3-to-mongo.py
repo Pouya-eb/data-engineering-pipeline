a2c363c2a31656471d237f26ff7efae47970e42f
vahid333 <vsh898@gmail.com>
2025-01-29 18:43:27 +0330

    -change dag to runall days

--- dags/s3-to-mongo.py
+++ dags/s3-to-mongo.py
@@ -8,17 +8,15 @@ from pymongo import MongoClient, errors
 import boto3
 from airflow.hooks.base import BaseHook
 from telegram_bot import Bot
-from airflow.decorators import dag  # Import the decorator
+from airflow.decorators import dag
 
 def telegram_callback(context):
     dag = context.get("dag")
     Bot().send_message(dag)
 
-# Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# DAG default arguments
 default_args = {
     "owner": "maj-vh",
     "on_failure_callback": telegram_callback,
@@ -28,7 +26,6 @@ default_args = {
     "retry_delay": timedelta(minutes=5),
 }
 
-# Create S3 client using environment variables
 def create_s3_client():
     return boto3.client(
         's3',
@@ -37,7 +34,6 @@ def create_s3_client():
         aws_secret_access_key=os.getenv('S3_SECRET_ACCESS_KEY')
     )
 
-# Create MongoDB client
 def create_mongo_client():
     connection_uri = BaseHook.get_connection("my_mongo_database").get_uri()
     
@@ -46,24 +42,26 @@ def create_mongo_client():
 
     return MongoClient(connection_uri)
 
-# Define the DAG using the @dag decorator
 @dag(
     dag_id='s3_to_mongodb_pipeline',
     description='Incremental Load from S3 to MongoDB',
-    schedule_interval='0 0 * * *',  # Runs daily at midnight
+    schedule_interval='0 0 * * *',
     default_args=default_args,
     catchup=False,
     tags=['Incremental', 'S3', 'MongoDB']
 )
 def s3_to_mongodb_dag():
-    # Task 1: List JSON files in S3
-    def list_s3_files(ti):
+    def list_s3_files(ti, execution_date):
         s3 = create_s3_client()
         bucket_name = os.getenv('S3_BUCKET_NAME')
 
+        if isinstance(execution_date, str):
+            execution_date = datetime.strptime(execution_date, "%Y-%m-%d")
+
+        target_date = execution_date.strftime("%Y-%m-%d")
+
         response = s3.list_objects_v2(Bucket=bucket_name)
 
-        target_date = "2025-01-22"
         json_files = [
             file['Key'] for file in response.get('Contents', [])
             if file['Key'].endswith(".json") and target_date in file['Key']
@@ -75,9 +73,9 @@ def s3_to_mongodb_dag():
     task_list_s3_files = PythonOperator(
         task_id='list_s3_files',
         python_callable=list_s3_files,
+        op_kwargs={'execution_date': '{{ ds }}'},
     )
 
-    # Task 2: Download JSON files from S3
     def download_s3_files(ti):
         json_files = ti.xcom_pull(task_ids='list_s3_files', key='json_files')
 
@@ -92,7 +90,6 @@ def s3_to_mongodb_dag():
         for file in json_files:
             file_id = os.path.basename(file)
 
-            # Skip if already processed
             if batch_collection.find_one({"file_name": file_id}):
                 logger.info(f"File already processed: {file_id}")
                 continue
@@ -107,7 +104,6 @@ def s3_to_mongodb_dag():
         python_callable=download_s3_files,
     )
 
-    # Task 3: Insert JSON data into MongoDB
     def insert_to_mongodb():
         mongo_client = create_mongo_client()
         db = mongo_client["mydb"]
@@ -125,7 +121,6 @@ def s3_to_mongodb_dag():
 
             file_path = os.path.join(path_write_json, file_name)
 
-            # Skip if already processed
             if batch_collection.find_one({"file_name": file_name}):
                 logger.info(f"Skipping already processed file: {file_name}")
                 continue
@@ -136,21 +131,20 @@ def s3_to_mongodb_dag():
                     for line in file:
                         try:
                             record = json.loads(line.strip())  
-                            records.append(record)  # Collect all records
-
+                            records.append(record)
                         except json.JSONDecodeError as e:
                             logger.warning(f"JSONDecodeError: {e} in file {file_name}, line: {line}")
                             continue
 
                     if records:
                         try:
-                            videos_collection.insert_many(records, ordered=False)  # Skip duplicates, continue inserting
+                            videos_collection.insert_many(records, ordered=False)
                             logger.info(f"Inserted {len(records)} records into 'videos'.")
                         except errors.BulkWriteError as bwe:
                             inserted_count = len([err for err in bwe.details["writeErrors"] if err["code"] != 11000])
                             logger.warning(f"BulkWriteError: {bwe.details}. Inserted {inserted_count} valid records.")
 
-                batch_collection.insert_one({"file_name": file_name})  # Mark file as processed
+                batch_collection.insert_one({"file_name": file_name})
                 logger.info(f"Processed and marked file as completed: {file_name}")
 
             except Exception as e:
@@ -161,8 +155,6 @@ def s3_to_mongodb_dag():
         python_callable=insert_to_mongodb,
     )
 
-    # Define task dependencies
     task_list_s3_files >> task_download_s3_files >> task_insert_to_mongodb
 
-# Instantiate the DAG
 dag = s3_to_mongodb_dag()
